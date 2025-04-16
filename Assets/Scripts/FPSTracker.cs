f60e681e2ac200ff707b9ea1b94ca766f25c4e20
KennyLi6 <kennyli3219@gmail.com>
2025-04-15 18:04:00 -0700

    basic fps counter created

---
+++ Assets/Scripts/FPSTracker.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class FPSTracker : MonoBehaviour
+{
+    [SerializeField] private Text counter;
+    int frameCounter = 0;
+    float timeCounter = 0.0f;
+    float lastFramerate = 0.0f;
+    public float refreshTime = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeCounter < refreshTime)
+        {
+            timeCounter += Time.deltaTime;
+            frameCounter++;
+        }
+        else
+        {
+            lastFramerate = (float)frameCounter / timeCounter;
+            frameCounter = 0;
+            timeCounter = 0.0f;
+        }
+        counter.text = lastFramerate.ToString();
+    }
+}
