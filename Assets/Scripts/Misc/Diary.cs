364345733c3f850e69db91edf3206abb7c049c06
Akash Basu <abasu924@gmail.com>
2025-06-02 16:03:33 -0700

    update overworld(my bad)

---
+++ Assets/Scripts/Misc/Diary.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diary : MonoBehaviour
+{
+    public List<string> found_diaries = new List<string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
