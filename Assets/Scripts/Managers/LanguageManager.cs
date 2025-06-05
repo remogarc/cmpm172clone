83d509d1558a43e4d3b30a3eedb2d77273fdd2fe
Akash Basu <abasu924@gmail.com>
2025-06-04 17:15:49 -0700

    finish adding dialogue to all NPCs

--- Assets/Scripts/Managers/LanguageManager.cs
+++ Assets/Scripts/Managers/LanguageManager.cs
@@ -61,6 +61,9 @@ public class LanguageManager : MonoBehaviour
             {"camera", "Camera"},
             {"audio", "Audio"},
             {"controls", "Controls"},
+            {"instructions", "W,A,S,D to move, E to interact\nRun in a circle to create an ink block!\nLshift to sprint\nSpacebar to jump\n\n\nAccessibility Mode: \nLclick to Move, Rclick to Jump \nPush scroll button to interact\nForward button to open pause menu\nBack button to sprint"},
+            {"acc_mode", "Accessibility Mode"},
+            {"music", "Music"},
             {"back", "Back"},
             {"resume", "Resume"},
             {"start", "Start"},
@@ -68,6 +71,10 @@ public class LanguageManager : MonoBehaviour
             {"quit", "Quit"},
             {"save", "Save"},
             {"load", "Load"},
+            {"pause", "Pause"},
+            {"well", "Wishing Well"},
+            {"ink", "Ink Block"},
+            {"esc", "Press 'esc'  or 'forward button' on mouse for pause menu"},
             {"x_sensitivity", "X Sensitivity"},
             {"y_sensitivity", "Y Sensitivity"},
             {"field_of_view", "Field of View"},
@@ -87,6 +94,9 @@ public class LanguageManager : MonoBehaviour
             {"camera", "Cámara"},
             {"audio", "Audio"},
             {"controls", "Controles"},
+            {"instructions", "W, A, S, D para moverte, E para interactuar.\n¡Corre en círculo para crear un bloque de tinta!\nShift izquierdo para correr.\nBarra espaciadora para saltar.\n\nModo de accesibilidad:\nClic izquierdo para moverte, clic derecho para saltar.\nPresiona el botón de desplazamiento para interactuar.\nAvanzar para abrir el menú de pausa.\nAtrás para correr."},
+            {"acc_mode", "Modo de accesibilidad"},
+            {"music", "Música"},
             {"back", "Atrás"},
             {"resume", "Continuar"},
             {"start", "Empezar"},
@@ -94,6 +104,10 @@ public class LanguageManager : MonoBehaviour
             {"quit", "Salir"},
             {"save", "Guardar"},
             {"load", "Cargar"},
+            {"pause", "Pausa"},
+            {"well", "Deseando Bien"},
+            {"ink", "Bloque de tinta"},
+            {"esc", "Presione 'esc' o 'botón de avance' en el mouse para el menú de pausa"},
             {"x_sensitivity", "Sensibilidad X"},
             {"y_sensitivity", "Sensibilidad Y"},
             {"field_of_view", "Campo de Visión"},
@@ -113,6 +127,9 @@ public class LanguageManager : MonoBehaviour
             {"camera", "Caméra"},
             {"audio", "Audio"},
             {"controls", "Contrôles"},
+            {"instructions", "W, A, S, D pour se déplacer, E pour interagir\nCourez en cercle pour créer un bloc dencre!\nMaj gauche pour sprinter\nBarre despace pour sauter\nMode daccessibilité:\nClic gauche pour se déplacer, clic droit pour sauter\nAppuyez sur le bouton de défilement pour interagir\nBouton Suivant pour ouvrir le menu Pause\nBouton Retour pour sprinter"},
+            {"acc_mode", "Mode d'accessibilité"},
+            {"music", "Musique"},            
             {"back", "Retour"},
             {"resume", "Reprendre"},
             {"start", "Commencer"},
@@ -120,6 +137,10 @@ public class LanguageManager : MonoBehaviour
             {"quit", "Quitter"},
             {"save", "Sauvegarder"},
             {"load", "Charger"},
+            {"pause", "Pause"},
+            {"well", "Souhaitant Bien"},
+            {"ink", "Bloc d'encre"},
+            {"esc", "Appuyez sur « esc » ou sur le bouton « avance » de la souris pour le menu de pause"},
             {"x_sensitivity", "Sensibilité X"},
             {"y_sensitivity", "Sensibilité Y"},
             {"field_of_view", "Champ de Vision"},
@@ -160,7 +181,106 @@ public class LanguageManager : MonoBehaviour
                 "If you ever want to know more about this world, let me know!"
             }
         };
-        
+        dialogueTranslations["English"]["joe"] = new DialogueTranslation
+        {
+            name = "Joe",
+            sentences = new string[]
+            {
+                "....?",
+                "You've seen me before?",
+                "Ohhh you must mean my twin brother Jeff!",
+                "Yeah I hate that guy. We had a falling out a couple of weeks ago, so I moved out."
+            }
+        };
+        dialogueTranslations["English"]["jay"] = new DialogueTranslation
+        {
+            name = "Jay",
+            sentences = new string[]
+            {
+                "...",
+                "I come here every year to mourn my wife....",
+                "I live in the village to the left of here....",
+                "It hasn't been easy, but it's getting better bit by bit...."
+            }
+        };
+        dialogueTranslations["English"]["villager1"] = new DialogueTranslation
+        {
+            name = "Villager 1",
+            sentences = new string[]
+            {
+                "If you walk to the right of here, we have a cemetery to mourn some of the dead village elders from here. ",
+                "We've never had an inkeeper live in this village though. They say that if an inkeeper dies, black ink starts floating in the place that they died. ",
+                "Legend has it that an inkeeper died here, we had a wishing well built in the exact place where they died. "
+            }
+        };
+        dialogueTranslations["English"]["villager2"] = new DialogueTranslation
+        {
+            name = "Villager 2",
+            sentences = new string[]
+            {
+                "The Inkeepers had a leader they called their king.",
+                "But one day as the inkeepers were dying the king was nowhere to be found. ",
+                "Many have searched for him, but all have failed."
+            }
+        };
+        dialogueTranslations["English"]["villager3"] = new DialogueTranslation
+        {
+            name = "Villager 2",
+            sentences = new string[]
+            {
+                "...",
+                "I called my teacher 'mom' today.... ",
+                "I can never recover from this level of embarassment.....",
+                "All the kids were laughing at me...."
+            }
+        };
+        dialogueTranslations["English"]["villager4"] = new DialogueTranslation
+        {
+            name = "Villager 1",
+            sentences = new string[]
+            {
+                "Legend has it the inkeepers built hidden structures that are buried here....",
+                "Archaelogists have tried searching for them, but no one has found anything...",
+                "I wonder where they could be....",
+                "Maybe they need some sort of key...? Like a book...?"
+            }
+        };
+        dialogueTranslations["English"]["villager5"] = new DialogueTranslation
+        {
+            name = "Villager 3",
+            sentences = new string[]
+            {
+                "Apparently if you press 'E' twice next to this fountain, you can save your progress.",
+                "What's that? How do I know? ",
+                "...",
+                "I don't know man try it for yourself..."
+            }
+        };
+        dialogueTranslations["English"]["villager6"] = new DialogueTranslation
+        {
+            name = "Villager 0",
+            sentences = new string[]
+            {
+                "....",
+                "This giant pillar right here is the grave of the king...   ",
+                "No one was able to find him during the inkeepers' time of need... Many believe that he is dead... So they built this tomb in his honor...",
+                "Me personally? I think he is dead too... The king loved his people... I refuse to believe that he'd willingly leave us when we needed him most... "
+            }
+        };
+        dialogueTranslations["English"]["villager7"] = new DialogueTranslation
+        {
+            name = "Villager 1",
+            sentences = new string[]
+            {
+                "Inkeepers used to live in this village...",
+                "But they are all extinct....",
+                "Now it's just me....",
+                "I'm very lonely... Sometimes I take rocks, dress them up, and draw faces on them... They're my only friends now...  "
+            }
+        };
+
+
+
         dialogueTranslations["Spanish"]["jeff"] = new DialogueTranslation
         {
             name = "Jeff",
@@ -171,7 +291,108 @@ public class LanguageManager : MonoBehaviour
                 "¡Si alguna vez quieres saber más sobre este mundo, házmelo saber!"
             }
         };
-        
+        dialogueTranslations["Spanish"]["joe"] = new DialogueTranslation
+        {
+            name = "Joe",
+            sentences = new string[]
+            {
+                "....?",
+                "¿Me has visto antes?",
+                "¡Ohhh, debes referirte a mi hermano gemelo Jeff!",
+                "Sí, odio a ese tipo. Tuvimos una pelea hace un par de semanas, así que me mudé."
+            }
+        };
+        dialogueTranslations["Spanish"]["jay"] = new DialogueTranslation
+        {
+            name = "Jay",
+            sentences = new string[]
+            {
+                "...",
+                "Vengo aquí todos los años a llorar a mi esposa...",
+                "Vivo en el pueblo a la izquierda de aquí...",
+                "No ha sido fácil, pero va mejorando poco a poco..."
+            }
+        };        
+        dialogueTranslations["Spanish"]["villager1"] = new DialogueTranslation
+        {
+            name = "Villager 1",
+            sentences = new string[]
+            {
+                "Si caminas a la derecha de aquí, tenemos un cementerio para recordar a algunos de los ancianos fallecidos de la aldea.",
+
+                "Aunque nunca hemos tenido un posadero viviendo en este pueblo. Dicen que si un posadero muere, la tinta negra empieza a flotar en el lugar donde murió.",
+
+                "Cuenta la leyenda que un posadero murió aquí; hicimos construir un pozo de los deseos en el mismo lugar donde murió."
+            }
+        };
+        dialogueTranslations["Spanish"]["villager2"] = new DialogueTranslation
+        {
+            name = "Villager 2",
+            sentences = new string[]
+            {
+                "Los guardianes tenían un líder al que llamaban su rey.",
+
+                "Pero un día, mientras los guardianes morían, el rey desapareció.",
+
+                "Muchos lo han buscado, pero todos han fracasado."
+            }
+        };
+        dialogueTranslations["Spanish"]["villager3"] = new DialogueTranslation
+        {
+            name = "Villager 2",
+            sentences = new string[]
+            {
+                "...", 
+                "Hoy llamé 'mamá' a mi maestra...", 
+                "Nunca podré recuperarme de este nivel de vergüenza...", 
+                "Todos los niños se reían de mí..."
+            }
+        };
+        dialogueTranslations["Spanish"]["villager4"] = new DialogueTranslation
+        {
+            name = "Villager 1",
+            sentences = new string[]
+            {
+                "Cuenta la leyenda que los guardianes construyeron estructuras ocultas que están enterradas aquí...", "Los arqueólogos han intentado buscarlas, pero nadie ha encontrado nada...", "Me pregunto dónde estarán...", "Quizás necesiten algún tipo de llave... ¿Algo así como un libro...?"
+            }
+        };
+        dialogueTranslations["Spanish"]["villager5"] = new DialogueTranslation
+        {
+            name = "Villager 3",
+            sentences = new string[]
+            {
+                "Parece que si pulsas 'E' dos veces junto a esta fuente, puedes guardar tu progreso.", "¿Qué es eso? ¿Cómo lo sé?",
+                "...",
+                "No lo sé, amigo, pruébalo tú mismo..."
+            }
+        };
+        dialogueTranslations["Spanish"]["villager6"] = new DialogueTranslation
+        {
+            name = "Villager 0",
+            sentences = new string[]
+            {
+                "....",
+                "Este pilar gigante de aquí es la tumba del rey...",
+                "Nadie pudo encontrarlo durante el tiempo de necesidad de los guardianes... Muchos creen que está muerto... Así que construyeron esta tumba en su honor...",
+                "¿Yo? Creo que él también está muerto... El rey amaba a su pueblo... Me niego a creer que nos abandonara voluntariamente cuando más lo necesitábamos..."
+            }
+        };
+        dialogueTranslations["Spanish"]["villager7"] = new DialogueTranslation
+        {
+            name = "Villager 1",
+            sentences = new string[]
+            {
+                "En este pueblo vivían posaderos...",
+                "Pero todos se extinguieron...",
+                "Ahora solo estoy yo...",
+                "Me siento muy solo... A veces tomo piedras, las decoro y les dibujo caras... Ahora son mis únicos amigos..."
+            }
+        };
+
+
+
+
+
         dialogueTranslations["French"]["jeff"] = new DialogueTranslation
         {
             name = "Jeff",
@@ -182,6 +403,107 @@ public class LanguageManager : MonoBehaviour
                 "Si vous voulez en savoir plus sur ce monde, faites-le moi savoir !"
             }
         };
+        dialogueTranslations["French"]["joe"] = new DialogueTranslation
+        {
+            name = "Joe",
+            sentences = new string[]
+            {
+                "....?",
+                "Tu m'as déjà vu ?",
+                "Ohhh, tu dois parler de mon frère jumeau Jeff !",
+                "Ouais, je déteste ce type. On s'est disputés il y a quelques semaines, alors j'ai déménagé."
+            }
+        };
+        dialogueTranslations["French"]["jay"] = new DialogueTranslation
+        {
+            name = "Jay",
+            sentences = new string[]
+            {
+                "...",
+                "Je viens ici chaque année pour pleurer ma femme...",
+                "J'habite dans le village à gauche d'ici...",
+                "Ça n'a pas été facile, mais ça s'améliore petit à petit..."
+            }
+        };  
+        dialogueTranslations["French"]["villager1"] = new DialogueTranslation
+        {
+            name = "Villager 1",
+            sentences = new string[]
+            {
+                "Si vous arrivez au droit d'ici, tenemos un cementerio para recordar a algunos de los ancianos fallecidos de la aldea.", 
+
+                "Aunque nunca hemos tenido un posadero viviendo dans ce pueblo. Dicen que si un posadero muere, la tinta negra empieza a flotar in el lugar donde murió.", 
+
+                "Cuenta la leyenda qu'un posadero murió aquí; nous avons construit un pozo de los deseos en el mismo lugar donde murió."
+            }
+        };
+        dialogueTranslations["French"]["villager2"] = new DialogueTranslation
+        {
+            name = "Villager 2",
+            sentences = new string[]
+            {
+                "Les intendants avaient un chef qu'ils appelaient leur roi.",
+                "Mais un jour, alors que les intendants mouraient, le roi était introuvable.",
+                "Nombreux sont ceux qui l'ont cherché, mais tous ont échoué."
+            }
+        };
+        dialogueTranslations["French"]["villager3"] = new DialogueTranslation
+        {
+            name = "Villager 2",
+            sentences = new string[]
+            {
+                "...", 
+                "J'ai appelé ma prof 'maman' aujourd'hui....", 
+                "Je ne pourrai jamais me remettre d'un tel niveau d'embarras.....", 
+                "Tous les enfants se moquaient de moi...."
+            }
+        };
+        dialogueTranslations["French"]["villager4"] = new DialogueTranslation
+        {
+            name = "Villager 1",
+            sentences = new string[]
+            {
+                " La légende raconte que les aubergistes ont construit des structures cachées qui sont enterrées ici… ",
+                " Les archéologues ont essayé de les chercher, mais personne n'a rien trouvé… ",
+                " Je me demande où ils peuvent bien être… ",
+                " Peut-être ont-ils besoin d'une sorte de clé… ? Comme un livre… ? "
+            }
+        };
+        dialogueTranslations["French"]["villager5"] = new DialogueTranslation
+        {
+            name = "Villager 3",
+            sentences = new string[]
+            {
+                "Apparemment, si tu appuies deux fois sur « E » à côté de cette fontaine, tu peux sauvegarder ta progression. ",
+                "Qu'est-ce que c'est ? Comment je le sais ?",
+                " … ",
+                " Je ne sais pas, mec, essaie par toi-même… "
+            }
+        };
+        dialogueTranslations["French"]["villager6"] = new DialogueTranslation
+        {
+            name = "Villager 0",
+            sentences = new string[]
+            {
+                "....",
+                "Ce pilier géant, là, est le tombeau du roi… ",
+                "Personne n'a pu le retrouver pendant la période difficile des gardiens… Beaucoup pensent qu'il est mort… Alors, ils ont construit ce tombeau en son honneur… ",
+                "Moi personnellement ? Je pense qu'il est mort aussi… Le roi aimait son peuple… Je refuse de croire qu'il nous aurait quittés volontairement au moment où nous avions le plus besoin de lui… "
+            }
+        };
+        dialogueTranslations["French"]["villager7"] = new DialogueTranslation
+        {
+            name = "Villager 1",
+            sentences = new string[]
+            {
+                "Il y avait des aubergistes dans ce village… ",
+                " Mais ils ont tous disparu… ",
+                " Maintenant, il n'y a plus que moi… ",
+                " Je suis très seul… Parfois, je prends des pierres, je les décore et je dessine des visages dessus… Ce sont mes seuls amis maintenant… "
+            }
+        };
+
+
         
         // Old Book dialogue
         dialogueTranslations["English"]["old_book"] = new DialogueTranslation
